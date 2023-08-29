2bf9cd2fe0070c5b55bccddbda4bac597761ebbe
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2023-08-29 05:35:37 +0500

    cleaned up code

--- solderingStation.ino
+++ solderingStation.ino
@@ -35,7 +35,7 @@ enum menuItems
     PRESETS
 };
 
-enum presets 
+enum presets
 {
     SOLDERING,
     HEATGUN,
@@ -65,7 +65,6 @@ bool EncoderButtonState = false;
 
 void setup()
 {
-
     pinMode(ENCODER_BUTTON, INPUT_PULLUP);
 
     pinMode(HEATGUN_FAN, OUTPUT);
@@ -78,7 +77,7 @@ void setup()
 
     display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS); // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
 
-    animatePikachu();
+    animatePikachu(2);
 
     display.clearDisplay();
     display.setTextSize(2); // Draw 2X-scale text
@@ -88,98 +87,9 @@ void setup()
 void loop()
 {
 
-    if (digitalRead(ENCODER_BUTTON)==LOW) {
-      delay(100);
-      if (digitalRead(ENCODER_BUTTON)==LOW) {
-        updateButtonState();
-      }
-    }
+    logic();
 
-    if(currentMenuItem==SOLDERING_IRON_SET_TEMP) {
-        solderingIronSetTemp += encoder.readAndReset()/2;
-    }
-    else if (currentMenuItem == HEATGUN_ELEMENT_SET_TEMP)
-    {
-        heatGunSetTemp += encoder.readAndReset()/2;
-    }
-    else if (currentMenuItem == HEATGUN_FAN_SET_SPEED)
-    {
-        heatGunFanSetSpeed += encoder.readAndReset()/2;
-        if (heatGunFanSetSpeed >= 255)
-        {
-            heatGunFanSetSpeed = 254;
-        }
-        if (heatGunFanSetSpeed <= 0)
-        {
-            heatGunFanSetSpeed = 0;
-        }
-    }
-
-    celsius0 = tcouple0.readTempC();
-    celsius1 = tcouple1.readTempC();
-
-    if (celsius0 < solderingIronSetTemp)
-    {
-        analogWrite(SOLDERING_IRON, 255);
-    }
-    else
-    {
-        analogWrite(SOLDERING_IRON, 0);
-    }
-    if (celsius1 < heatGunSetTemp)
-    {
-        analogWrite(HEATGUN_ELEMENT, 255);
-    }
-    else
-    {
-        analogWrite(HEATGUN_ELEMENT, 0);
-    }
-
-    analogWrite(HEATGUN_FAN, heatGunFanSetSpeed);
-
-    display.clearDisplay();
-    display.drawFastVLine(64, 0, 54, SSD1306_WHITE);
-    display.drawFastVLine(50, 54, 10, SSD1306_WHITE);
-    display.drawFastHLine(0, 54, 128, SSD1306_WHITE);
-    display.setTextSize(1);
-    display.setCursor(0, 0);
-    display.println("Set Temp:");
-    display.setCursor(69, 0);
-    display.println("Set Temp:");
-    display.setCursor(0, 26);
-    display.println("Temp:");
-    display.setCursor(69, 26);
-    display.println("Temp:");
-    display.setCursor(0, 56);
-    display.println("Fan: ");
-    if (currentMenuItem == HEATGUN_FAN_SET_SPEED)
-    {
-        display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
-    }
-    display.setCursor(25, 56);
-    display.println(String((int)((((float)heatGunFanSetSpeed/fanTopPWM)*100))) + "%");
-    display.setTextColor(SSD1306_WHITE);
-
-    display.setTextSize(2);
-    if (currentMenuItem == SOLDERING_IRON_SET_TEMP)
-    {
-        display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
-    }
-    display.setCursor(0, 10);
-    display.println(solderingIronSetTemp);
-    display.setTextColor(SSD1306_WHITE);
-    if (currentMenuItem == HEATGUN_ELEMENT_SET_TEMP)
-    {
-        display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
-    }
-    display.setCursor(69, 10);
-    display.println(heatGunSetTemp);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0, 36);
-    display.println((int)(celsius0));
-    display.setCursor(69, 36);
-    display.println((int)(celsius1));
-    display.display();
+    graphics();
 }
 
 void animatePikachu()
@@ -213,26 +123,126 @@ void animatePikachu(int repeat, int speed)
 void updateButtonState()
 {
 
+    switch (currentMenuItem)
+    {
+    case NOTHING:
+        currentMenuItem = SOLDERING_IRON_SET_TEMP;
+        break;
+    case SOLDERING_IRON_SET_TEMP:
+        currentMenuItem = HEATGUN_ELEMENT_SET_TEMP;
+        break;
+    case HEATGUN_ELEMENT_SET_TEMP:
+        currentMenuItem = HEATGUN_FAN_SET_SPEED;
+        break;
+    case HEATGUN_FAN_SET_SPEED:
+        currentMenuItem = PRESETS;
+        break;
+    case PRESETS:
+        currentMenuItem = NOTHING;
+    default:
+        currentMenuItem = NOTHING;
+        break;
+    }
+}
 
-        switch (currentMenuItem)
+void graphics()
+{
+    display.clearDisplay();
+    display.drawFastVLine(64, 0, 54, SSD1306_WHITE);
+    display.drawFastVLine(50, 54, 10, SSD1306_WHITE);
+    display.drawFastHLine(0, 54, 128, SSD1306_WHITE);
+    display.setTextSize(1);
+    display.setCursor(0, 0);
+    display.println("Set Temp:");
+    display.setCursor(69, 0);
+    display.println("Set Temp:");
+    display.setCursor(0, 26);
+    display.println("Temp:");
+    display.setCursor(69, 26);
+    display.println("Temp:");
+    display.setCursor(0, 56);
+    display.println("Fan: ");
+    if (currentMenuItem == HEATGUN_FAN_SET_SPEED)
+    {
+        display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+    }
+    display.setCursor(25, 56);
+    display.println(String((int)((((float)heatGunFanSetSpeed / fanTopPWM) * 100))) + "%");
+    display.setTextColor(SSD1306_WHITE);
+
+    display.setTextSize(2);
+    if (currentMenuItem == SOLDERING_IRON_SET_TEMP)
+    {
+        display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+    }
+    display.setCursor(0, 10);
+    display.println(solderingIronSetTemp);
+    display.setTextColor(SSD1306_WHITE);
+    if (currentMenuItem == HEATGUN_ELEMENT_SET_TEMP)
+    {
+        display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+    }
+    display.setCursor(69, 10);
+    display.println(heatGunSetTemp);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 36);
+    display.println((int)(celsius0));
+    display.setCursor(69, 36);
+    display.println((int)(celsius1));
+    display.display();
+}
+
+void logic()
+{
+    if (digitalRead(ENCODER_BUTTON) == LOW)
+    {
+        delay(100);
+        if (digitalRead(ENCODER_BUTTON) == LOW)
         {
-        case NOTHING:
-            currentMenuItem = SOLDERING_IRON_SET_TEMP;
-            break;
-        case SOLDERING_IRON_SET_TEMP:
-            currentMenuItem = HEATGUN_ELEMENT_SET_TEMP;
-            break;
-        case HEATGUN_ELEMENT_SET_TEMP:
-            currentMenuItem = HEATGUN_FAN_SET_SPEED;
-            break;
-        case HEATGUN_FAN_SET_SPEED:
-            currentMenuItem = PRESETS;
-            break;
-        case PRESETS:
-            currentMenuItem = NOTHING;
-        default:
-            currentMenuItem = NOTHING;
-            break;
+            updateButtonState();
         }
+    }
 
+    if (currentMenuItem == SOLDERING_IRON_SET_TEMP)
+    {
+        solderingIronSetTemp += encoder.readAndReset() / 2;
+    }
+    else if (currentMenuItem == HEATGUN_ELEMENT_SET_TEMP)
+    {
+        heatGunSetTemp += encoder.readAndReset() / 2;
+    }
+    else if (currentMenuItem == HEATGUN_FAN_SET_SPEED)
+    {
+        heatGunFanSetSpeed += encoder.readAndReset() / 2;
+        if (heatGunFanSetSpeed >= 255)
+        {
+            heatGunFanSetSpeed = 254;
+        }
+        if (heatGunFanSetSpeed <= 0)
+        {
+            heatGunFanSetSpeed = 0;
+        }
+    }
+
+    celsius0 = tcouple0.readTempC();
+    celsius1 = tcouple1.readTempC();
+
+    if (celsius0 < solderingIronSetTemp)
+    {
+        analogWrite(SOLDERING_IRON, 255);
+    }
+    else
+    {
+        analogWrite(SOLDERING_IRON, 0);
+    }
+    if (celsius1 < heatGunSetTemp)
+    {
+        analogWrite(HEATGUN_ELEMENT, 255);
+    }
+    else
+    {
+        analogWrite(HEATGUN_ELEMENT, 0);
+    }
+
+    analogWrite(HEATGUN_FAN, heatGunFanSetSpeed);
 }
